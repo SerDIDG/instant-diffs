1f6949cc0e72caf072cc68c68f91abc1048d98a2
Serhio Magpie <serdidg@gmail.com>
2025-07-29 08:44:36 +0300

    Follows defensive programming when executing extrenal script for the Global Watchlist. Renred expiry status icon in the watchlist when updating watch \ unwatch status. Updated language strings and language documentation.

--- src/watchstar.js
+++ src/watchstar.js
@@ -8,20 +8,44 @@ import Api from './Api';
 const notificationId = 'mw-watchlink-notification';
 
 /**
+ * Gets how many expiry days left in the watchlist.
+ * @param {string} expiry
+ * @return {number|null}
+ */
+function getDaysLeftExpiry( expiry ) {
+    if ( !expiry || mw.util.isInfinity( expiry ) ) return null;
+
+    const expiryDate = new Date( expiry );
+    const currentDate = new Date();
+
+    // Using the Math.ceil function instead of floor so when, for example, a user selects one week
+    // the tooltip shows 7 days instead of 6 days (see Phab ticket T253936)
+    return Math.ceil( ( expiryDate - currentDate ) / ( 1000 * 60 * 60 * 24 ) );
+}
+
+/**
  * Adds or removes page from the watchlist.
  * @param {import('./Article').default} article an Article instance
  * @param {OO.ui.ButtonWidget} button a OO.ui.ButtonWidget instance
  * @returns {mw.Api.AbortablePromise}
  */
-export function setWatchStatus( article, button ) {
+export async function setWatchStatus( article, button ) {
+    await Api.loadMessage( [
+        'watchlist-expiring-days-full-text',
+        'watchlist-expiring-hours-full-text',
+        'tooltip-ca-watch',
+        'tooltip-ca-unwatch',
+        'tooltip-ca-unwatch-expiring',
+        'tooltip-ca-unwatch-expiring-hours',
+    ] );
+
     preloadWatchNotice( article );
 
-    const preferredExpiry = mw.user.options.get( 'watchstar-expiry', 'infinity' );
-    const watched = article.get( 'watched' );
     const hostname = article.get( 'hostname' );
     const title = article.getMW( 'title' ).getPrefixedDb();
+    const preferredExpiry = mw.user.options.get( 'watchstar-expiry', 'infinity' );
 
-    const request = watched
+    const request = article.get( 'watched' )
         ? Api.unwatch( title, hostname )
         : Api.watch( title, preferredExpiry, hostname );
 
@@ -91,7 +115,7 @@ function showWatchNotice( article, button, response ) {
     // only if Watchlist Expiry is enabled
     if ( isWatchlistExpiryEnabled ) {
         if ( isWatched ) {
-            if ( !preferredExpiry || mw.util.isInfinity( preferredExpiry ) ) {
+            if ( mw.util.isInfinity( preferredExpiry ) ) {
                 // The message should include `infinite` watch period
                 message = mwTitle.isTalkPage() ? 'addedwatchindefinitelytext-talk' : 'addedwatchindefinitelytext';
             } else {
@@ -158,8 +182,8 @@ function showWatchNotice( article, button, response ) {
  * @param {import('./Article').default} article an Article instance
  * @param {OO.ui.ButtonWidget} button a OO.ui.ButtonWidget instance
  * @param {mw.Title|JQuery<HTMLElement>} titleOrLink
- * @param {string} action
- * @param {string} state
+ * @param {('watch', 'unwatch')} action
+ * @param {('idle', 'loading')} state
  * @param {string} expiry
  * @param {string} expirySelected
  */
@@ -184,17 +208,21 @@ function updateWatchStatus( article, button, [ titleOrLink, action, state, expir
         updatePageWatchStatus?.( watched, expiry, expirySelected );
     }
 
-    // For the watchlist, also update watchlist lines
-    if (
-        !article.isForeign &&
-        mw.user.options.get( 'watchlistunwatchlinks' ) && id.local.mwCanonicalSpecialPageName === 'Watchlist'
-    ) {
-        updateWatchlistStatus( article, watched, expiry, expirySelected );
-    }
+    // Perform next updates only on the idle state
+    if ( state !== 'loading' ) {
+        // For the watchlist, also update watchlist lines.
+        if (
+            !article.isForeign &&
+            mw.user.options.get( 'watchlistunwatchlinks' ) &&
+            id.local.mwCanonicalSpecialPageName === 'Watchlist'
+        ) {
+            updateWatchlistStatus( article, watched, expiry );
+        }
 
-    // For the global watchlist, also update watchlist lines
-    if ( id.local.mwCanonicalSpecialPageName === 'GlobalWatchlist' ) {
-        updateGlobalWatchlistStatus( article, watched, expiry, expirySelected );
+        // For the global watchlist, also update watchlist lines.
+        if ( id.local.mwCanonicalSpecialPageName === 'GlobalWatchlist' ) {
+            updateGlobalWatchlistStatus( article, watched, expiry );
+        }
     }
 }
 
@@ -209,11 +237,11 @@ export function updateWatchLinkStatus( article, button ) {
     const watched = article.get( 'watched' );
     const action = watched ? 'unwatch' : 'watch';
     const expiry = article.get( 'expiry' ) || 'infinity';
+    const daysLeftExpiry = getDaysLeftExpiry( expiry );
 
     const label = `action-${ action }`;
     const href = mw.util.getUrl( article.get( 'title' ), { action } );
 
-    let daysLeftExpiry = null;
     let tooltipAction;
     let icon;
 
@@ -223,12 +251,6 @@ export function updateWatchLinkStatus( article, button ) {
             tooltipAction = 'unwatch';
             icon = 'unStar';
         } else {
-            const expiryDate = new Date( expiry );
-            const currentDate = new Date();
-
-            // Using the Math.ceil function instead of floor so when, for example, a user selects one week
-            // the tooltip shows 7 days instead of 6 days (see Phab ticket T253936)
-            daysLeftExpiry = Math.ceil( ( expiryDate - currentDate ) / ( 1000 * 60 * 60 * 24 ) );
             tooltipAction = daysLeftExpiry > 0 ? 'unwatch-expiring' : 'unwatch-expiring-hours';
             icon = 'halfStar';
         }
@@ -254,11 +276,10 @@ export function updateWatchLinkStatus( article, button ) {
  * @param {import('./Article').default} article an Article instance
  * @param {boolean} watched
  * @param {string} expiry
- * @param {string} expirySelected
  */
-export function updateWatchlistStatus( article, watched, expiry, expirySelected ) {
+export function updateWatchlistStatus( article, watched, expiry ) {
     if ( watched ) {
-        forEachWatchlistMatchingTitle( article.get( 'titleText' ), ( rowTitle, $row, $link ) => {
+        forEachWatchlistLines( article.get( 'titleText' ), ( rowTitle, $row, $link ) => {
             $link
                 .text( mw.msg( 'watchlist-unwatch' ) )
                 .attr( 'title', mw.msg( 'tooltip-ca-unwatch' ) )
@@ -271,27 +292,10 @@ export function updateWatchlistStatus( article, watched, expiry, expirySelected 
                 .addBack( '.mw-enhanced-rc-nested' )
                 .removeClass( 'mw-changelist-line-inner-unwatched' );
 
-            $row
-                .find( '.mw-changesList-watchlistExpiry' )
-                .each( ( i, node ) => {
-                    const $expiry = $( node );
-
-                    // Add the missing semicolon (T266747)
-                    $expiry
-                        .next( '.mw-changeslist-separator' )
-                        .addClass( 'mw-changeslist-separator--semicolon' )
-                        .removeClass( 'mw-changeslist-separator' );
-
-                    // Remove the spaces before and after the expiry icon
-                    node.nextSibling.nodeValue = node.nextSibling.nodeValue.trimStart();
-                    node.previousSibling.nodeValue = node.previousSibling.nodeValue.trimEnd();
-
-                    // Remove the icon
-                    $expiry.remove();
-                } );
+            updateWatchlistExpiryStatus( $row, watched, expiry );
         } );
     } else {
-        forEachWatchlistMatchingTitle( article.get( 'titleText' ), ( rowTitle, $row, $link ) => {
+        forEachWatchlistLines( article.get( 'titleText' ), ( rowTitle, $row, $link ) => {
             $link
                 .text( mw.msg( 'watchlist-unwatch-undo' ) )
                 .attr( 'title', mw.msg( 'tooltip-ca-watch' ) )
@@ -308,20 +312,123 @@ export function updateWatchlistStatus( article, watched, expiry, expirySelected 
 }
 
 /**
+ * Updates watch / unwatch status in the Watchlist expiry icon.
+ * @param {JQuery<HTMLElement>} $row
+ * @param {boolean} watched
+ * @param {string} expiry
+ */
+function updateWatchlistExpiryStatus( $row, watched, expiry ) {
+    if ( !watched ) return;
+
+    // Collect all visible expiry icons
+    const $expiry = $row.find( '.mw-changesList-watchlistExpiry' );
+
+    // Remove expiry icons when expiry is infinite
+    if ( mw.util.isInfinity( expiry ) ) {
+        return removeWatchlistExpiryStatus( $row, $expiry );
+    }
+
+    const daysLeftExpiry = getDaysLeftExpiry( expiry );
+    const tooltipAction = daysLeftExpiry > 0 ? 'days-full-text' : 'hours-full-text';
+    const message = mw.msg( `watchlist-expiring-${ tooltipAction }`, daysLeftExpiry );
+
+    if ( $expiry.length > 0 ) {
+        // Change title of the existing icons
+        $expiry.each( ( i, node ) => {
+            node.title = message;
+        } );
+    } else {
+        // Render new icons
+        renderWatchlistExpiryStatus( $row, message );
+    }
+}
+
+/**
+ * Removes expiry icon in the Watchlist.
+ * @param {JQuery<HTMLElement>} $row
+ * @param {JQuery<HTMLElement>}  $expiry
+ */
+function removeWatchlistExpiryStatus( $row, $expiry ) {
+    $expiry.each( ( i, node ) => {
+        const $this = $( node );
+
+        // Replace separator to semicolon (T266747)
+        $this
+            .next( '.mw-changeslist-separator' )
+            .addClass( 'mw-changeslist-separator--semicolon' )
+            .removeClass( 'mw-changeslist-separator' );
+
+        // Remove the spaces before and after the expiry icon
+        node.previousSibling.nodeValue = node.previousSibling.nodeValue.trimEnd();
+        node.nextSibling.nodeValue = node.nextSibling.nodeValue.trimStart();
+
+        // Remove the icon
+        $expiry.remove();
+    } );
+
+    // Add white-space for the grouped lists
+    $row
+        .find( '.mw-changeslist-line-inner-historyLink' )
+        .prepend( ' ' );
+}
+
+/**
+ * Renders new OO.ui expiry icon in the Watchlist.
+ * @param {JQuery<HTMLElement>} $row
+ * @param {string}  message
+ */
+function renderWatchlistExpiryStatus( $row, message ) {
+    $row
+        .find( '.mw-title' )
+        .each( ( i, node ) => {
+            const $this = $( node );
+
+            const $reviewLink = $this
+                .next( '.mw-fr-reviewlink' );
+
+            const icon = new OO.ui.IconWidget( {
+                icon: 'clock',
+                title: message,
+                classes: [ 'mw-changesList-watchlistExpiry' ],
+            } );
+
+            ( $reviewLink.length > 0 ? $reviewLink : $this )
+                .after( ' ' )
+                .after( icon.$element )
+                .after( ' ' );
+
+            // Replace semicolon to separator (T266747)
+            icon.$element
+                .next( '.mw-changeslist-separator--semicolon' )
+                .addClass( 'mw-changeslist-separator' )
+                .removeClass( 'mw-changeslist-separator--semicolon' );
+        } );
+}
+
+/**
+ * Callback for utility function.
+ * @callback forEachWatchlistLinesCallback
+ * @param {string} ttile
+ * @param {JQuery<HTMLElement>} $row
+ * @param {JQuery<HTMLElement>} $link
+ */
+
+/**
  * Utility function for looping through each watchlist line that matches
  * a certain page or its associated page (e.g. Talk).
  * Partially copied from:
  * {@link https://gerrit.wikimedia.org/g/mediawiki/core/+/9c590c2c37434ca7a2bd101b547ccf7dcc46b538/resources/src/mediawiki.special.watchlist/watchlist.js#81}
  * @param {string} title
- * @param {Function} callback
+ * @param {forEachWatchlistLinesCallback} callback
  */
-function forEachWatchlistMatchingTitle( title, callback ) {
+function forEachWatchlistLines( title, callback ) {
     const mwTitle = mw.Title.newFromText( title );
     const associatedMwTitle = mwTitle.isTalkPage() ? mwTitle.getSubjectPage() : mwTitle.getTalkPage();
     const associatedTitle = associatedMwTitle.getPrefixedText();
 
     $( '.mw-changeslist-line' ).each( ( i, node ) => {
         const $line = $( node );
+
         $line.find( '[data-target-page]' ).each( ( i, node ) => {
             const $this = $( node );
             const rowTitle = String( $this.data( 'targetPage' ) );
@@ -332,29 +439,43 @@ function forEachWatchlistMatchingTitle( title, callback ) {
                 // * If using OldChangesList, use the <li>
                 // * If using EnhancedChangesList and $this is part of a grouped log entry, use the <td> sub-entry
                 // * If using EnhancedChangesList and $this is not part of a grouped log entry, use the <table> grouped entry
-                const $row = $this.closest(
-                    'li, .mw-enhancedchanges-checkbox + table.mw-changeslist-log td[data-target-page], table',
-                );
-                const $link = $row.find( '.mw-unwatch-link, .mw-watch-link' );
+                const $row = $this
+                    .closest( 'li, .mw-enhancedchanges-checkbox + table.mw-changeslist-log td[data-target-page], table' );
+
+                const $link = $row
+                    .find( '.mw-unwatch-link, .mw-watch-link' );
+
                 callback( rowTitle, $row, $link );
             }
         } );
     } );
 }
 
+/******* GLOBAL WATCHLIST *******/
+
 /**
  * Updates watch / unwatch status in the Global Watchlist.
  * @param {import('./Article').default} article an Article instance
  * @param {boolean} watched
  * @param {string} expiry
- * @param {string} expirySelected
  */
-export function updateGlobalWatchlistStatus( article, watched, expiry, expirySelected ) {
+export function updateGlobalWatchlistStatus( article, watched, expiry ) {
     if ( !mw.globalwatchlist ) return;
 
-    const watchedSites = mw.globalwatchlist.watchedSites.siteList.find( entry => entry.site === article.get( 'hostname' ) );
-    if ( !watchedSites ) return;
+    // Follows defensive programming fot the external scripts
+    try {
+        const watchedSites = mw.globalwatchlist.watchedSites.siteList
+            .find( entry => entry.site === article.get( 'hostname' ) );
+        if ( !watchedSites ) return;
 
-    // Use origTitle value instead of title or titleText, because article's title is formatted with canonical prefixes
-    watchedSites.processUpdateWatched( article.get( 'origTitle' ), !watched );
+        // Use origTitle value instead of title or titleText,
+        // because article's title is formatted with canonical prefixes,
+        // but extension is expected title in the local format .
+        watchedSites.processUpdateWatched( article.get( 'origTitle' ), !watched );
+    } catch ( error ) {
+        utils.notifyError( 'error-global-watchlist', {
+            type: 'watchstar',
+            message: error?.message || error,
+        }, article, true );
+    }
 }
