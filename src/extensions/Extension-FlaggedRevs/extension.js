fbb638ac74750abdbe5b817bbaec99313824e46d
Serhio Magpie <serdidg@gmail.com>
2026-08-10 01:30:04 +0300

    Added enhancements to the "Revision Review" special page: - marks the diff list lines as ready to be processed.

--- src/extensions/Extension-FlaggedRevs/extension.js
+++ src/extensions/Extension-FlaggedRevs/extension.js
@@ -1,12 +1,27 @@
 /**
  * EXTENSION: FLAGGED REVS
  *
- * Requests FlaggedRevs form and renders review button.
+ * Adds enhancements to the "Revision Review" special page:
+ * - marks the diff list lines as ready to be processed.
+ * Adds enhancements to the Page view:
+ * - requests FlaggedRevs form and renders review button.
  * @see {@link https://www.mediawiki.org/wiki/Extension:FlaggedRevs}
  */
 
 import id from '../../id';
+import * as utils from '../../utils';
+
 import ReviewForm from './ReviewForm';
+
+/**
+ * Marks the diff list lines as ready to be processed.
+ */
+function processRevisionReview() {
+	const $container = utils.getContentNode();
+	$container
+		.find( 'ul li' )
+		.attr( 'data-instantdiffs-line', 'all' );
+}
 
 mw.hook( `${ id.config.prefix }.page.renderSuccess` ).add(
 	/**
@@ -17,3 +32,10 @@ mw.hook( `${ id.config.prefix }.page.renderSuccess` ).add(
 		new ReviewForm( page );
 	},
 );
+
+mw.hook( `${ id.config.prefix }.pageAdjustments` ).add( ( id ) => {
+	// Process Special:RevisionReview
+	if ( id.local.mwCanonicalSpecialPageName === 'RevisionReview' ) {
+		processRevisionReview();
+	}
+} );
